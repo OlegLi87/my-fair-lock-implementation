cadd1f45ad2f1f842ac0606e744fd533497a3317
Oleg Li <34588348+OlegLi87@users.noreply.github.com>
2017-12-21 14:44:19 +0200

    Update MyFairLockImplementation.java

--- MyFairLockImplementation.java
+++ MyFairLockImplementation.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MyFairLockImplementation {
 	
